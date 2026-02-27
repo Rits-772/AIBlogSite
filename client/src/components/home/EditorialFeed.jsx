99cffc8751db6d2c562a0ef0040d088e889b0e88
Ritvik Sharma <shub7r2@gmail.com>
2026-02-27 20:34:19 +0530

    supabase errors fixed

--- client/src/components/home/EditorialFeed.jsx
+++ client/src/components/home/EditorialFeed.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import { posts as postsApi } from "../../utils/api";
 import { getFallbackImage } from "../../utils/assets";
 
